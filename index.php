79c9b73f1028b15f47b07ce48f597be0f387dce4
gdc21 <developers.gdc.2021@gmail.com>
2022-08-12 15:59:18 -0500

    10 Update Michoacan

--- index.php
+++ index.php
@@ -3,8 +3,10 @@ session_start();
 //die;
 //Necesario para localizar la carpeta padre y generar el CONTEXT
 $carpetaPadre = dirname(__FILE__);
+
 //El archivo Init, configura todas las rutas.
 include_once 'conf/Init.php';
+
 // Incluimos el archivo Route.php para empezar la ejecucion del sistema
 include_once 'conf/Route.php';
 
@@ -14,7 +16,9 @@ include_once 'conf/Helpers.php';
 #SistemaDeCache::getInstance()->hazAlgo(); // Uso de sistema de cache
 #SistemaDeCache::getInstance()->limpiarTodaLaCache();
 include_once 'conf/SistemaDeCache.php';
+
 // Se crea un objeto
 $route = new Route;
+
 // Se llama al metodo action
 $route -> action();
