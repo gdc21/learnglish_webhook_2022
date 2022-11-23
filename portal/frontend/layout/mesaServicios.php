2cafaccfd0919b4008c13415d114bdae54325513
OmarJhon9872 <omarjonax0@gmail.com>
2022-11-23 17:17:08 -0600

    47 Se agrego nueva mesa de servicio para campeche, coahuila, principal, bcs, michoacan y tamaulipas, id de mesa de servicio esta en tabla 43 en columna lgf04300002 como MesaServicios

--- portal/frontend/layout/mesaServicios.php
+++ portal/frontend/layout/mesaServicios.php
@@ -1,52 +1,20 @@
-<?php if (!isset($_SESSION['userLogged'])) { ?>
+<?php if (verificaModuloSistemaRetornoValor("MesaServicios") != '' && isset($_SESSION['userLogged'])) { ?>
     <script>
-        window.fwSettings = {
-            'widget_id': <?php echo verificaModuloSistemaRetornoValor("MesaServicios"); ?>
-        };
-        ! function() {
-            if ("function" != typeof window.FreshworksWidget) {
-                var n = function() {
-                    n.q.push(arguments)
-                };
-                n.q = [], window.FreshworksWidget = n
+        window.addEventListener('mouseover', initLandbot, { once: true });
+        window.addEventListener('touchstart', initLandbot, { once: true });
+        var myLandbot;
+        function initLandbot() {
+            if (!myLandbot) {
+                var s = document.createElement('script');s.type = 'text/javascript';s.async = true;
+                s.addEventListener('load', function() {
+                    var myLandbot = new Landbot.Livechat({
+                        configUrl: 'https://storage.googleapis.com/landbot.pro/v3/<?php echo verificaModuloSistemaRetornoValor("MesaServicios"); ?>/index.json',
+                    });
+                });
+                s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
             }
-        }()
-    </script>
-    <script type='text/javascript' src='https://widget.freshworks.com/widgets/<?php echo verificaModuloSistemaRetornoValor("MesaServicios"); ?>.js' async defer></script>
-<?php } else { ?>
-    <script src="https://wchat.freshchat.com/js/widget.js"></script>
-    <script>
-        (function(d, w, c) {
-            if (!d.getElementById("spd-busns-spt")) {
-                var n = d.getElementsByTagName('script')[0],
-                    s = d.createElement('script');
-                var loaded = false;
-                s.id = "spd-busns-spt";
-                s.async = "async";
-                s.setAttribute("data-self-init", "false");
-                s.setAttribute("data-init-type", "opt");
-                s.src = 'https://cdn.freshbots.ai/assets/share/js/freshbots.min.js';
-                s.setAttribute("data-client", "14668a18fc951dd5fb84ee191bf7646e280c8f2b");
-                s.setAttribute("data-bot-hash", "c23fc1079a624f88043be455e7e8847155366c81");
-                s.setAttribute("data-env", "prod");
-                s.setAttribute("data-region", "us");
-                if (c) {
-                    s.onreadystatechange = s.onload = function() {
-                        if (!loaded) {
-                            c();
-                        }
-                        loaded = true;
-                    };
-                }
-                n.parentNode.insertBefore(s, n);
-            }
-        })(document, window, function() {
-            Freshbots.initiateWidget({
-                autoInitChat: false,
-                getClientParams: function() {
-                    return;
-                }
-            }, function(successResponse) {}, function(errorResponse) {});
-        });
+        }
     </script>
 <?php } ?>
