0832c71f1d14cdaef458e091aea7b4011bed5f0c
OmarJhon9872 <omarjonax0@gmail.com>
2022-11-24 09:25:22 -0600

    48 Correccion mesa de servicio

--- portal/frontend/layout/mesaServicios.php
+++ portal/frontend/layout/mesaServicios.php
@@ -1,4 +1,4 @@
-<?php if (verificaModuloSistemaRetornoValor("MesaServicios") != '' && isset($_SESSION['userLogged'])) { ?>
+<?php if (verificaModuloSistemaRetornoValor("MesaServicios") != '' && !isset($_SESSION['userLogged'])) { ?>
     <script>
         window.addEventListener('mouseover', initLandbot, { once: true });
         window.addEventListener('touchstart', initLandbot, { once: true });
@@ -9,6 +9,25 @@
                 s.addEventListener('load', function() {
                     var myLandbot = new Landbot.Livechat({
                         configUrl: 'https://storage.googleapis.com/landbot.pro/v3/<?php echo verificaModuloSistemaRetornoValor("MesaServicios"); ?>/index.json',
+                    });
+                });
+                s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
+            }
+        }
+    </script>
+<?php }elseif(verificaModuloSistemaRetornoValor("MesaServicios") != '' && isset($_SESSION['userLogged'])){ ?>
+    <script>
+        window.addEventListener('mouseover', initLandbot, { once: true });
+        window.addEventListener('touchstart', initLandbot, { once: true });
+        var myLandbot;
+        function initLandbot() {
+            if (!myLandbot) {
+                var s = document.createElement('script');s.type = 'text/javascript';s.async = true;
+                s.addEventListener('load', function() {
+                    var myLandbot = new Landbot.Livechat({
+                        configUrl: 'https://storage.googleapis.com/landbot.pro/v3/H-1416677-8ALUYID2IOFI08JP/index.json',
                     });
                 });
                 s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
